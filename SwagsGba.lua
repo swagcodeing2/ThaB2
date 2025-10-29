5a1edff7dfad47002e98ed0c8211a023fb8674ac
swagcodeing2 <200179763+swagcodeing2@users.noreply.github.com>
2025-10-29 16:41:38 -0700

    Update SwagsGba.lua with loading message

---
+++ SwagsGba.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/swagcodeing2/ThaB1/refs/heads/main/swaggba.lua'))()
+-- dont u dare u skid u a bitch if u do
+print("Diddys Script Loaded By Swag")
